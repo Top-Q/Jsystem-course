cf70038585114cd5b4c8a65c84fc892c8ddbe0bb
itaiag <itai.agmon@gmail.com>
2015-02-03 15:23:35 +0200

    Solution 1

--- file-system-tests/src/main/java/org/jsystem/tutorial/file_system_tests/LocalFileSystemOperations.java
+++ file-system-tests/src/main/java/org/jsystem/tutorial/file_system_tests/LocalFileSystemOperations.java
@@ -1,6 +1,11 @@
 package org.jsystem.tutorial.file_system_tests;
 
+import java.io.File;
+import java.io.IOException;
+
 import junit.framework.SystemTestCase4;
+
+import org.junit.Test;
 
 /**
  * Provides building blocks for performing file system operations on local
@@ -10,5 +15,33 @@ import junit.framework.SystemTestCase4;
  *
  */
 public class LocalFileSystemOperations extends SystemTestCase4 {
-	
+
+	private String prefix, suffix;
+
+	/**
+	 * Create temporary file with the specified prefix and suffix. Return the
+	 * full path of the newly created file.
+	 * 
+	 */
+	@Test
+	public void createTempFile() throws IOException {
+		File.createTempFile(prefix, suffix);
+	}
+
+	public String getPrefix() {
+		return prefix;
+	}
+
+	public void setPrefix(String prefix) {
+		this.prefix = prefix;
+	}
+
+	public String getSuffix() {
+		return suffix;
+	}
+
+	public void setSuffix(String suffix) {
+		this.suffix = suffix;
+	}
+
 }
