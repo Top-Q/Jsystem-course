e9b2ad85ee20bff402ee6fbc3c8deaed061c9d72
itaiag <itai.agmon@gmail.com>
2015-02-03 15:21:21 +0200

    Exercise 1

---
+++ file-system-tests/src/main/java/org/jsystem/tutorial/file_system_tests/LocalFileSystemOperations.java
@@ -0,0 +1,14 @@
+package org.jsystem.tutorial.file_system_tests;
+
+import junit.framework.SystemTestCase4;
+
+/**
+ * Provides building blocks for performing file system operations on local
+ * machine
+ * 
+ * @author Itai Agmon
+ *
+ */
+public class LocalFileSystemOperations extends SystemTestCase4 {
+	
+}
