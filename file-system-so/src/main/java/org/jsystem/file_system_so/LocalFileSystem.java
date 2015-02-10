0190d4dbcc8aad19e0c0b6fc5b02f2f5b2a2342c
itaiag <itai.agmon@gmail.com>
2015-02-10 14:23:34 +0200

    Sol6

--- file-system-so/src/main/java/org/jsystem/file_system_so/LocalFileSystem.java
+++ file-system-so/src/main/java/org/jsystem/file_system_so/LocalFileSystem.java
@@ -3,25 +3,54 @@ package org.jsystem.file_system_so;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.CopyOption;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 
+import jsystem.framework.report.ReporterHelper;
 import jsystem.framework.system.SystemObjectImpl;
 
 public class LocalFileSystem extends SystemObjectImpl {
-	
+
 	/**
 	 * The location of the file repository folder. This is the folder that will
 	 * be used in the <i>copyFileFromTheRepository</i> method
 	 */
 	private String repositoryFolder;
-	
+
 	public String createTempFile(String prefix, String suffix) throws IOException {
-		return null;
+		report.step("About to create temporary file");
+		String tempFile = File.createTempFile(prefix, suffix).getAbsolutePath();
+		report.report("Created file with name: " + tempFile);
+		return tempFile;
 	}
 
 	/**
 	 * Create multiple files according to user preferences.
 	 */
 	public void createMultipleFiles(FileAttributes[] fileAttributesArr) throws Exception {
+		report.step("About to create multiple files");
+		if (null == fileAttributesArr || fileAttributesArr.length == 0) {
+			report.report("No file attributes were specified by user", 2);
+			return;
+		}
+		report.startLevel("Crating multiple files");
+		try {
+			for (FileAttributes attr : fileAttributesArr) {
+				Path filePath = Paths.get(attr.getFolder() + "/" + attr.getName());
+				report.report("About to create file " + filePath);
+				Files.write(filePath, attr.getContent().getBytes(), StandardOpenOption.CREATE);
+				File file = new File(filePath.toString());
+				if (attr.isReadOnly()) {
+					file.setReadOnly();
+				}
+				ReporterHelper.copyFileToReporterAndAddLink(report, file, attr.getName());
+			}
+
+		} finally {
+			report.stopLevel();
+		}
 	}
 
 	/**
@@ -29,7 +58,10 @@ public class LocalFileSystem extends SystemObjectImpl {
 	 * 
 	 */
 	public void writeToFile(File file, String content, boolean append) throws IOException {
-
+		report.step("About to write content to file");
+		report.report("File content", content, true);
+		final Path filePath = Paths.get(file.getAbsolutePath());
+		Files.write(filePath, content.getBytes(), append ? StandardOpenOption.APPEND : StandardOpenOption.CREATE);
 	}
 
 	/**
@@ -37,16 +69,21 @@ public class LocalFileSystem extends SystemObjectImpl {
 	 * you can specify additional options.
 	 */
 	public void copyFile(File sourceFile, File destinationFile, CopyOption copyOption) throws IOException {
-
+		report.step("About to copy file");
+		Files.copy(Paths.get(sourceFile.getAbsolutePath()), Paths.get(destinationFile.getAbsolutePath()), copyOption);
 	}
-
-
 
 	public void copyFileFromRepository(String fileFromRepository, File destinationFile, CopyOption copyOption)
 			throws IOException {
+		report.step("About to copy file from the repository");
+		Path sourcePath = Paths.get(repositoryFolder + "/" + fileFromRepository);
+		Path destinationPath = Paths.get(destinationFile.getAbsolutePath());
+		if (copyOption != null) {
+			Files.copy(sourcePath, destinationPath, copyOption);
+		} else {
+			Files.copy(sourcePath, destinationPath);
+		}
 	}
-
-
 
 	public String getRepositoryFolder() {
 		return repositoryFolder;
@@ -55,7 +92,5 @@ public class LocalFileSystem extends SystemObjectImpl {
 	public void setRepositoryFolder(String repositoryFolder) {
 		this.repositoryFolder = repositoryFolder;
 	}
-	
-	
-	
+
 }
---
+++ file-system-tests/src/main/java/org/jsystem/tutorial/file_system_tests/LocalFileSystemOperationsAdvanced.java
@@ -0,0 +1,227 @@
+package org.jsystem.tutorial.file_system_tests;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.StandardCopyOption;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import jsystem.framework.ParameterProperties;
+import jsystem.framework.TestProperties;
+import jsystem.framework.scenario.Parameter;
+import jsystem.framework.scenario.UseProvider;
+import jsystem.framework.scenario.ValidationError;
+import junit.framework.SystemTestCase4;
+
+import org.jsystem.file_system_so.FileAttributes;
+import org.jsystem.file_system_so.LocalFileSystem;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Provides building blocks for performing file system operations on local
+ * machine
+ * 
+ * @author Itai Agmon
+ *
+ */
+public class LocalFileSystemOperationsAdvanced extends SystemTestCase4 {
+
+
+	private StandardCopyOption copyOption;
+
+	private String tempFile, prefix, suffix, content, fileFromRepository;
+
+	private File file, sourceFile, destinationFile;
+
+	private boolean append, emptyBeforeDeleting;
+
+	private FileAttributes[] fileAttributesArr;
+
+	private LocalFileSystem fileSystem;
+
+	@Before
+	public void setUp() throws Exception {
+		fileSystem = (LocalFileSystem) system.getSystemObject("fileSystem");
+	}
+
+	/**
+	 * Create temporary file with the specified prefix and suffix. Return the
+	 * full path of the newly created file.
+	 * 
+	 */
+	@Test
+	@TestProperties(name = "Local - Create temp file with prefix '${prefix}' and suffix '${suffix}'", paramsInclude = {
+			"prefix", "suffix" }, returnParam = { "tempFile" })
+	public void createTempFile() throws IOException {
+		tempFile = fileSystem.createTempFile(prefix, suffix);
+	}
+
+	/**
+	 * Create multiple files according to user preferences.
+	 */
+	@Test
+	@TestProperties(name = "Local - Create multiple files", paramsInclude = { "fileAttributesArr" })
+	public void createMultipleFiles() throws Exception {
+		fileSystem.createMultipleFiles(fileAttributesArr);
+	}
+
+	/**
+	 * Write specified content to file.
+	 * 
+	 */
+	@Test
+	@TestProperties(name = "Local - Write content to file '${file}'", paramsInclude = { "file", "content", "append" })
+	public void writeToFile() throws IOException {
+		fileSystem.writeToFile(file, content, append);
+	}
+
+	/**
+	 * Copy the specified source file to the specified destination. Notice that
+	 * you can specify additional options.
+	 */
+	@Test
+	@TestProperties(name = "Local - Copy file '${sourceFile}' to '${destinationFile}'", paramsInclude = { "sourceFile",
+			"destinationFile", "copyOption" })
+	public void copyFile() throws IOException {
+		fileSystem.copyFile(sourceFile, destinationFile, copyOption);
+	}
+
+
+
+	@Test
+	@TestProperties(name = "Local - Copy file '${fileFromRepository}' to '${destinationFile}'", paramsInclude = {
+			"fileFromRepository", "destinationFile", "copyOption" })
+	public void copyFileFromRepository() throws IOException {
+		fileSystem.copyFileFromRepository(fileFromRepository, destinationFile, copyOption);
+	}
+
+
+
+	@Override
+	public ValidationError[] validate(HashMap<String, Parameter> map, String methodName) throws Exception {
+		List<ValidationError> veList = new ArrayList<ValidationError>();
+		if (methodName.equals("copyFile")) {
+			if (map.get("SourceFile").getValue() == null || map.get("SourceFile").getValue().toString().isEmpty()) {
+				ValidationError ve = new ValidationError();
+				ve.setTitle("Source file is not specified");
+				veList.add(ve);
+			}
+			if (map.get("DestinationFile").getValue() == null
+					|| map.get("DestinationFile").getValue().toString().isEmpty()) {
+				ValidationError ve = new ValidationError();
+				ve.setTitle("Destination file is not specified");
+				veList.add(ve);
+			}
+		}
+		return veList.toArray(new ValidationError[] {});
+	}
+
+
+
+	/**** Setters and Getters method ****/
+
+	public String getTempFile() {
+		return tempFile;
+	}
+
+	public void setTempFile(String tempFile) {
+		this.tempFile = tempFile;
+	}
+
+	public String getPrefix() {
+		return prefix;
+	}
+
+	public void setPrefix(String prefix) {
+		this.prefix = prefix;
+	}
+
+	public String getSuffix() {
+		return suffix;
+	}
+
+	public void setSuffix(String suffix) {
+		this.suffix = suffix;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public File getFile() {
+		return file;
+	}
+
+	public void setFile(File file) {
+		this.file = file;
+	}
+
+	public boolean isAppend() {
+		return append;
+	}
+
+	public void setAppend(boolean append) {
+		this.append = append;
+	}
+
+
+	public File getSourceFile() {
+		return sourceFile;
+	}
+
+	@ParameterProperties(description = "Source file")
+	public void setSourceFile(File sourceFile) {
+		this.sourceFile = sourceFile;
+	}
+
+	public File getDestinationFile() {
+		return destinationFile;
+	}
+
+	public void setDestinationFile(File destinationFile) {
+		this.destinationFile = destinationFile;
+	}
+
+	public StandardCopyOption getCopyOption() {
+		return copyOption;
+	}
+
+	@ParameterProperties(section = "Additional Options")
+	public void setCopyOption(StandardCopyOption copyOption) {
+		this.copyOption = copyOption;
+	}
+
+	public String getFileFromRepository() {
+		return fileFromRepository;
+	}
+
+	public void setFileFromRepository(String fileFromRepository) {
+		this.fileFromRepository = fileFromRepository;
+	}
+
+	public FileAttributes[] getFileAttributesArr() {
+		return fileAttributesArr;
+	}
+
+	@UseProvider(provider = jsystem.extensions.paramproviders.ObjectArrayParameterProvider.class)
+	public void setFileAttributesArr(FileAttributes[] fileAttributesArr) {
+		this.fileAttributesArr = fileAttributesArr;
+	}
+
+	public boolean isEmptyBeforeDeleting() {
+		return emptyBeforeDeleting;
+	}
+
+	public void setEmptyBeforeDeleting(boolean emptyBeforeDeleting) {
+		this.emptyBeforeDeleting = emptyBeforeDeleting;
+	}
+
+	/*************/
+
+}
